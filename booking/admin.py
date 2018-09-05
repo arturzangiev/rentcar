427cdf21388a8d65cd55bc5911f69c3e83a9205c
arturzangiev <zangiev@gmail.com>
2018-09-05 19:53:55 +0100

    Car model is created

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'car_make', 'car_model', 'car_registration', 'car_vin')
+    # prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(models.Car, CarAdmin)
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    car_make = models.CharField(max_length=100)
+    car_model = models.CharField(max_length=100)
+    car_registration = models.CharField(max_length=50)
+    car_vin = models.CharField(max_length=50)
+
+
+    # h1_tag = models.CharField(max_length=50, null=True, blank=True)
+    # description = models.TextField(max_length=500, null=True, blank=True)
+    # slug = models.SlugField(unique=True)
+    # seo_title = models.CharField(max_length=100, null=True, blank=True)
+    # seo_description = models.CharField(max_length=200, null=True, blank=True)
+    # seo_primary_keyword = models.CharField(max_length=50, null=True, blank=True)
+    # seo_secondary_keyword = models.CharField(max_length=50, null=True, blank=True)
+    # twitter_account = models.CharField(max_length=15, null=True, blank=True) #todo change the field type
+    # twitter_tag = models.CharField(max_length=15, null=True, blank=True) #todo change the field type
+    # facebook_page = models.CharField(max_length=15, null=True, blank=True)  # todo change the field type
+    # facebook_group = models.CharField(max_length=15, null=True, blank=True)  # todo change the field type
+
+    def __str__(self):
+        return self.car_registration
