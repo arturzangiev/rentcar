6af32495aa547e4a5f0e4f7840698ebe101d07a1
arturzangiev <zangiev@gmail.com>
2018-09-05 20:14:35 +0100

    Booking model is created

--- booking/admin.py
+++ booking/admin.py
@@ -4,7 +4,11 @@ from . import models
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('id', 'car_make', 'car_model', 'car_registration', 'car_vin')
-    # prepopulated_fields = {'slug': ('name',)}
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'car', 'first_name', 'last_name', 'start_date', 'end_date')
 
 
 admin.site.register(models.Car, CarAdmin)
+admin.site.register(models.Booking, BookingAdmin)
--- booking/models.py
+++ booking/models.py
@@ -7,18 +7,16 @@ class Car(models.Model):
     car_registration = models.CharField(max_length=50)
     car_vin = models.CharField(max_length=50)
 
-
-    # h1_tag = models.CharField(max_length=50, null=True, blank=True)
-    # description = models.TextField(max_length=500, null=True, blank=True)
-    # slug = models.SlugField(unique=True)
-    # seo_title = models.CharField(max_length=100, null=True, blank=True)
-    # seo_description = models.CharField(max_length=200, null=True, blank=True)
-    # seo_primary_keyword = models.CharField(max_length=50, null=True, blank=True)
-    # seo_secondary_keyword = models.CharField(max_length=50, null=True, blank=True)
-    # twitter_account = models.CharField(max_length=15, null=True, blank=True) #todo change the field type
-    # twitter_tag = models.CharField(max_length=15, null=True, blank=True) #todo change the field type
-    # facebook_page = models.CharField(max_length=15, null=True, blank=True)  # todo change the field type
-    # facebook_group = models.CharField(max_length=15, null=True, blank=True)  # todo change the field type
-
     def __str__(self):
         return self.car_registration
+
+
+class Booking(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.first_name
