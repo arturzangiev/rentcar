b50bcb5611613a068d159efb091a381f93ea2e8d
arturzangiev <zangiev@gmail.com>
2018-09-07 16:54:04 +0100

    Created slots check

--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Car, Booking
 from .forms import BookingCreateForm
-
+import datetime
+from datetime import timedelta
 
 def index(request):
     if request.user.is_authenticated:
@@ -14,23 +16,31 @@ def index(request):
     template = 'index.html'
     return render(request, template, context)
 
+
 @login_required(login_url='/')
 def create(request):
     if request.method == "POST":
         form = BookingCreateForm(request.POST)
         if form.is_valid():
-            # Check two hours
-            # bookings = Booking.objects.all().filter(user=request.user)
+            user = request.user
+            start_date = form.cleaned_data.get('start_date')
+            end_date = form.cleaned_data.get('end_date')
+            car = form.cleaned_data.get('car')
 
+            # Check if start_date is less then end_date
+            if start_date < end_date:
+                q_start_date = start_date - timedelta(hours=2)
+                q_end_date = end_date + timedelta(hours=2)
+                bookings = Booking.objects.all().filter(Q(car=car) & Q(start_date__range=[q_start_date, q_end_date]) | Q(end_date__range=[q_start_date, q_end_date])).count()
+                if bookings == 0:
+                    Booking.objects.create(user=user, car=car, start_date=start_date, end_date=end_date)
+                    return HttpResponseRedirect('/create/')
 
-            obj = Booking.objects.create(
-                user=request.user,
-                car=form.cleaned_data.get('car'),
-                start_date=form.cleaned_data.get('start_date'),
-                end_date=form.cleaned_data.get('end_date')
-            )
+                else:
+                    return HttpResponse("The slot is already booked")
 
-            return HttpResponseRedirect('/create/')
+            else:
+                return HttpResponseRedirect('/create/')
 
     if request.method == "GET":
         cars = Car.objects.all()
