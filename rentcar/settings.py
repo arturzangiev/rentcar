5768ddafb1c485797abef7312760e2a912ddcd3f
arturzangiev <zangiev@gmail.com>
2018-09-07 22:41:15 +0100

    Static folder is created

--- rentcar/settings.py
+++ rentcar/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '../booking/static'
 
 LOGIN_REDIRECT_URL = '/create/'
 LOGOUT_REDIRECT_URL = '/'
